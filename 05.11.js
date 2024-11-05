cdf4c1444ee837310c2399ee542309f67cec8d0a
kristina-per <pheradzekristina5@gmail.com>
2024-11-05 09:04:17 +0400

    Add files via upload

---
+++ 05.11.js
@@ -0,0 +1,24 @@
+let number = 11;
+if (number > 50) {
+    console.log ("more")
+} 
+else {
+    console.log( "Less" )
+}
+
+let number2 = 18 ;
+if (number2 == 0) {
+    console.log ("0")
+} 
+else (number < 0)
+{
+    console.log( "-" )
+}
+ 
+let number3 = 7; 
+squared = number3 ** number3
+if (squared > 100 )
+     console.log (">100")
+else {
+    console.log ("<100")
+}
